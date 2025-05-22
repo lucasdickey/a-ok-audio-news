dcc4efd1d11a4355d23a0b57c5be6737ac0aadca
Lucas Dickey <lucas@lucasdickey.com>
2025-05-21 18:41:45 -0700

    fixed settings and admin stuff

--- aok_audio_news/settings.py
+++ aok_audio_news/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Django REST Framework
     'digests',  # Add the digests app
 ]
 
